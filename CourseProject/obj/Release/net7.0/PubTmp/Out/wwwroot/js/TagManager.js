03fbad9a2d5c9ae3731297fedf439573e8e5a309
Клим <@DESKTOP-0EQI3IF>
2024-10-24 23:23:45 +0300

    refactor: change the connection string

---
+++ CourseProject/obj/Release/net7.0/PubTmp/Out/wwwroot/js/TagManager.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    let tagIndex = 0;
+    $("#add-tag-button").click(function () {
+        var tagValue = $("#tag-input").val().trim();
+        if (tagValue) {
+            addTag(tagValue);
+            $("#tag-input").val(''); // Clear the input
+        }
+    });
+
+
+    function addTag(tag)
+    {
+        let tagExists = false;
+
+        // Check if the tag is already in the list by comparing input values
+        $("#selected-tags li").each(function () {
+            // Get the value of the hidden input in the current <li>
+            const inputValue = $(this).find('input[type="hidden"]').val().trim();
+
+            // Compare trimmed input value with the new tag
+            if (inputValue === tag.trim()) {
+                tagExists = true; // Tag already exists
+                return false; // Exit loop early
+            }
+        });
+
+
+        if (!tagExists)
+        {
+            $("#selected-tags").append(
+                '<li class="list-group-item">' +
+                '<input type="hidden" name="Tags[' + tagIndex + '].Name" value="' + tag + '" />' +
+                tag +
+                ' <button type="button" class="btn btn-danger btn-sm remove-tag">x</button>' +
+                '</li>'
+            );
+            tagIndex++;
+        }
+        else
+        {
+            alert("You can't input the same tag name");
+        }
+
+    }
+
+    $(document).on('click', '.remove-tag', function () {
+        $(this).parent().remove(); // Remove the tag from the list
+    });
+});
+	
---
+++ CourseProject/obj/Release/net7.0/PubTmp/Out/wwwroot/js/checkboxManager.js
@@ -0,0 +1,6 @@
+﻿document.getElementById('selectAll').addEventListener('change', function() {
+    var checkboxes = document.querySelectorAll('input[name="selectedItems"]');
+    checkboxes.forEach(function(checkbox) {
+        checkbox.checked = document.getElementById('selectAll').checked;
+    });
+});
